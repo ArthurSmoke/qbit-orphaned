ada5fc2d977889866a05b0aa999f4db4bc4c82ea
Arthur <fengfeng623@gmail.com>
2023-03-13 17:50:11 +0800

    Update orphaned.py

--- root/app/orphaned.py
+++ root/app/orphaned.py
@@ -12,12 +12,12 @@ PASS = os.environ['QBT_PASS']
 files_in_download_folder=[]
 print("Scanning Download Folder")
 for dir_path, _, file_names in os.walk(DOWNLOADS_PATH):
-	for file_name in file_names:
-		full_path = os.path.join(dir_path, file_name)
-		relative_path = os.path.relpath(full_path, DOWNLOADS_PATH)
+    for file_name in file_names:
+	full_path = os.path.join(dir_path, file_name)
+	relative_path = os.path.relpath(full_path, DOWNLOADS_PATH)
         # ignore the file that contain keyword
-		if not any(ignore_text in relative_path for ignore_text in IGNORE_LIST):
-		    files_in_download_folder.append(relative_path)
+	if not any(ignore_text in relative_path for ignore_text in IGNORE_LIST):
+	    files_in_download_folder.append(relative_path)
 
 # remove files used by torrents in qBit
 client = Client(host=HOST, username=USER, password=PASS)
